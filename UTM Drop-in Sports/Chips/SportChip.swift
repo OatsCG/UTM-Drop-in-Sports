d11c35c54f297e26cd6374317d0e0360bf9f0dcf
OatsCG <charlesg2003@hotmail.com>
2024-09-05 00:24:16 -0400

    Created chip views

---
+++ UTM Drop-in Sports/Chips/SportChip.swift
@@ -0,0 +1,44 @@
+//
+//  SportChip.swift
+//  UTM Drop-in Sports
+//
+//  Created by Charlie Giannis on 2024-09-05.
+//
+
+import SwiftUI
+
+struct SportChip: View {
+    var title: String
+    var image: String
+    @State var favourited: Bool = true
+    @State var selected: Bool = false
+    var body: some View {
+        Button(action: {
+            withAnimation(.interactiveSpring) {
+                selected.toggle()
+            }
+        }) {
+            HStack {
+                Image(systemName: image)
+                    .symbolEffect(.bounce, value: selected)
+                Text(title)
+                if favourited {
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                }
+            }
+                .foregroundStyle(selected ? .white : .black)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 8)
+                .background {
+                    Capsule().fill(selected ? .blue : .white)
+                        .stroke(.quaternary, lineWidth: 2)
+                }
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+#Preview {
+    SportChip(title: "Soccer", image: "soccerball")
+}
---
+++ UTM Drop-in Sports/Chips/SportChips.swift
@@ -0,0 +1,18 @@
+//
+//  SportChips.swift
+//  UTM Drop-in Sports
+//
+//  Created by Charlie Giannis on 2024-09-05.
+//
+
+import SwiftUI
+
+struct SportChips: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SportChips()
+}
